a31febaddee3dcdaacc4c20a2b92cd2911979eae
Cody Uhi <codylkuhi@gmail.com>
2021-05-26 00:05:54 -0600

    Finish Intro section styling & navbar

--- html-app/js/scripts.js
+++ html-app/js/scripts.js
@@ -1,8 +1,21 @@
 function toggleCourses(type) {
     const div = document.getElementById(type + '-list');
-    if(div.classList.contains('hidden')) {
+    if (div.classList.contains('hidden')) {
         div.classList.remove('hidden');
     } else {
         div.classList.add('hidden');
     }
 }
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var os = $('#intro').offset().top;
+        var ht = $('#intro').height();
+        if (scroll > os + ht) {
+            $('#page-navbar').addClass('navbar-background');
+        } else {
+            $('#page-navbar').removeClass('navbar-background');
+        }
+    });
+});
