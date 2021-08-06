05e591f2e7bc3a554b4556140c5c2233043b914d
Cody Uhi <codylkuhi@gmail.com>
2021-08-05 20:04:55 -0600

    Create Vue App

---
+++ vue-app/src/components/Content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="content-container"></div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ vue-app/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="header-container"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+</style>
